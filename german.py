65eb68ad64deeb58719c55ea07f409510b7c8236
German Burset Romero <150076592+germanbursetromero@users.noreply.github.com>
2024-06-02 10:11:01 -0400

    environment variable loading

--- german.py
+++ german.py
@@ -1,1 +1,11 @@
 # german's file
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+
+print(client_id, client_secret)
