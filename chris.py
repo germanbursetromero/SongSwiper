d05189f58ad052f7ffa436d37774440b10cb49e9
chris <cjmadden@loyola.edu>
2024-06-03 14:22:28 -0400

    chris changes

--- chris.py
+++ chris.py
@@ -1,2 +1,17 @@
-# chris' file
+#chris file
+"""
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+
+print (client_id)
+print (client_secret)
+
+print ("Hello world")
+"""
+
+print ("Hello World")
