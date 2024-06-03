a5dd154b103e7d7662e37da6eb18fda737b87dec
German Burset Romero <150076592+germanbursetromero@users.noreply.github.com>
2024-06-02 20:34:03 -0400

    setup basic redirects and oauth

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # german's file
 
 from flask import Flask, request, url_for, session, redirect
+from dotenv import load_dotenv
+import os
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
@@ -11,14 +13,22 @@ app.config['SESSION_COOKIE_NAME'] = 'Beach Cookie'
 
 @app.route('/')
 def login():
-    return "Song Swiper"
+    sp_oauth = create_spotify_oauth()
+    auth_url = sp_oauth.get_authorize_url()
+    return redirect(auth_url)
 
 @app.route('/redirect')
-def redirect():
+def redirectPage():
     return "redirect"
 
 @app.route('/getTracks')
 def getTracks():
     return "User top songs"
 
-
+def create_spotify_oauth():
+    return SpotifyOAuth(
+        client_id= "194564bfd0694b6c85aef9f8182616eb",
+        client_secret= "bf2ca6fa80af4a5e86dff533018a79b7",
+        redirect_uri=url_for('redirectPage', _external=True),
+        scope="user-library-read"
+    )
--- german.py
+++ german.py
@@ -4,6 +4,7 @@ from flask import Flask, request, url_for, session, redirect
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
+
 app = Flask(__name__)
 
 app.secret_key = "8fhkslfmpio4pa98"
