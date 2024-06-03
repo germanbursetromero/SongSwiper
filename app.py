7cd53ef363e58052a194b4fda0d865efadfe3018
German Burset Romero <150076592+germanbursetromero@users.noreply.github.com>
2024-06-02 22:37:39 -0400

    got oauth and saved tracks to work through flask and spotipy

--- app.py
+++ app.py
@@ -5,11 +5,14 @@ from dotenv import load_dotenv
 import os
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+import time
+import json
 
 app = Flask(__name__)
 
 app.secret_key = "8fhkslfmpio4pa98"
 app.config['SESSION_COOKIE_NAME'] = 'Beach Cookie'
+TOKEN_INFO = "token_info"
 
 @app.route('/')
 def login():
@@ -19,11 +22,88 @@ def login():
 
 @app.route('/redirect')
 def redirectPage():
-    return "redirect"
+    sp_oauth = create_spotify_oauth()
+    session.clear()
+    code = request.args.get('code')
+    token_info = sp_oauth.get_access_token(code)
+    session[TOKEN_INFO] = token_info
+    return redirect(url_for('chooseAction', _external=True))
+
+@app.route('/chooseAction')
+def chooseAction():
+    return """
+        <h1>Choose an action:</h1>
+        <ul>
+            <li><a href="/getTracks">View Saved Tracks</a></li>
+            <li><a href="/getTopTracks">View Top Tracks</a></li>
+        </ul>
+    """
 
 @app.route('/getTracks')
 def getTracks():
-    return "User top songs"
+    try:
+        token_info = get_token()
+    except:
+        print("User not logged in")
+        return redirect("/")
+    sp = spotipy.Spotify(auth=token_info["access_token"])
+    all_songs = []
+    iteration = 0
+    while True:
+        items = sp.current_user_saved_tracks(limit=50, offset=iteration * 50)["items"]
+        iteration += 1
+        all_songs += items
+        if(len(items) < 50):
+            break
+    
+    limited_songs = all_songs[:50]
+    formatted_songs = []
+
+    for song in limited_songs:
+        track = song['track']
+        track_name = track['name']
+        artists = ', '.join(artist['name'] for artist in track['artists'])
+        formatted_songs.append(f"{track_name} by {artists}")
+
+    return "<br>".join(formatted_songs)
+
+@app.route('/getTopTracks')
+def getTopTracks():
+    try:
+        token_info = get_token()
+    except:
+        print("User not logged in")
+        return redirect("/")
+    sp = spotipy.Spotify(auth=token_info["access_token"])
+    top_tracks = []
+    iteration = 0
+    while True:
+        items = sp.current_user_top_tracks(limit=10, offset=iteration * 10)["items"]
+        iteration += 1
+        top_tracks += items
+        if(len(items) < 50):
+            break
+    
+    formatted_tracks = []
+    for track in top_tracks:
+        track_name = track['name']
+        artists = ', '.join(artist['name'] for artist in track['artists'])
+        formatted_tracks.append(f"{track_name} by {artists}")
+
+    return "<br>".join(formatted_tracks)
+    
+
+def get_token():
+    token_info = session.get(TOKEN_INFO, None)
+    if not token_info:
+        raise "exception"
+    now = int(time.time())
+
+    is_expired = token_info["expires_at"] - now < 60
+    if(is_expired):
+        sp_oauth = create_spotify_oauth()
+        token_info = sp_oauth.refresh_access_token(token_info["refresh_token"])
+    return token_info
 
 def create_spotify_oauth():
     return SpotifyOAuth(
